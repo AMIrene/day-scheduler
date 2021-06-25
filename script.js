073828a7e72fd51473c49e525d5294893a6c9e17
AMIrene <f.adammanik@gmail.com>
2021-06-26 01:17:07 +1000

    local storage set on save button click

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
+
 //current day is displayed at the top of the calendar (in header element)
 
-const timeDate = document.getElementById("currentDay");
-setInterval(() => {
-    const current = moment();
-    const displayTimeDate = current.format("dddd, MMMM Do YYYY");
-   timeDate.textContent = displayTimeDate;
-}, 1000);
+//Vanilla JS
 
+// const timeDate = document.getElementById("currentDay");
+// setInterval(() => {
+//     const current = moment();
+//     const displayTimeDate = current.format("dddd, MMMM Do YYYY");
+//    timeDate.textContent = displayTimeDate;
+// }, 1000);
+
+//JQUERY
+
+let today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
 //Timeblock changes based on past, present and future
 //Past is grey
@@ -15,10 +22,63 @@ setInterval(() => {
 //Present is red
 
 
+
 //Future is green
 
+//User clicks calendar icon to save event in timeblock jquery
 
-//Click save button to save event for selected timeblock
+
+function saveEvent() {
+
+    $(".time-block").each(function () {
+        var timeOfday = $(this).attr("id");
+        var taskEntry = localStorage.getItem(timeOfday);
+
+        if (taskEntry !== null) {
+            $(this).children(".eventLog").val(taskEntry);
+        }
+    });
+}
+
+saveEvent();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var taskEntry = $(this).siblings(".eventLog").val();
+
+    //event is then saved in local storage
+
+    localStorage.setItem(time, taskEntry);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//User clicks calendar icon button to save event in timeblock vanilla js
+// function saveEvent(event) {
+//     event.preventDefault();
+//     console.log(event)
+
+// const element = event.target;
+// console.log(element.dataset.time);
+
+// }
+// saveBtnEls.forEach(function (element){
+//     element.addEventListener('click', saveEvent)
+
+// });
+
+
+
 //saved in local storage
 
 
